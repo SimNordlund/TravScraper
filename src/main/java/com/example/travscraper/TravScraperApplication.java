f55ce8f8da11cabc71bba4e1bdf4a7a4115bf7da
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-11-06 19:18:26 +0100

    Temp scraping future.

--- src/main/java/com/example/travscraper/TravScraperApplication.java
+++ src/main/java/com/example/travscraper/TravScraperApplication.java
@@ -22,6 +22,6 @@ public class TravScraperApplication implements CommandLineRunner {
     @Override
     public void run(String... args) {
         service.scrapeFuture();
-        service.scrape();
+      //  service.scrape();
     }
 }
