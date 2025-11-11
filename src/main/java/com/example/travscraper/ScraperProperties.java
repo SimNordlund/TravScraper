0e15259731606941ac4ba398c3121042982d8641
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2025-11-11 19:23:58 +0100

    Foreign properties added

--- src/main/java/com/example/travscraper/ScraperProperties.java
+++ src/main/java/com/example/travscraper/ScraperProperties.java
@@ -14,10 +14,12 @@ import java.time.LocalDate;
 public class ScraperProperties {
     //70-150 skrapade för results
     //71-0 för future
-    private LocalDate startDateFuture = LocalDate.now().minusDays(0);
+    private LocalDate startDateFuture = LocalDate.now().minusDays(4);
     private LocalDate endDateFuture = LocalDate.now().minusDays(0);
-    private LocalDate startDateResults = LocalDate.now().minusDays(1);
+    private LocalDate startDateResults = LocalDate.now().minusDays(4);
     private LocalDate endDateResults = LocalDate.now().minusDays(1);
+    private LocalDate startDateForeign = LocalDate.now().minusDays(4);
+    private LocalDate endDateResultForeign = LocalDate.now().minusDays(1);
 
 /*    private LocalDate startDateResults = LocalDate.now().minusDays(0);
     private LocalDate endDateResults = LocalDate.now().minusDays(0);
